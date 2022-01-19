79c2c5f792b8c6ac686f5d9e5a3cb5711e48ae85
Terry Kinder <terry.kinder@gmail.com>
2022-01-19 01:27:49 -0600

    update server.js to 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 corsProxy
   .createServer({
     originWhitelist: [
-      'http://localhost:4200',
+      'http://localhost:3000',
       'https://crypto-four.herokuapp.com/',
     ],
     requireHeader: ['origin', 'x-requested-with'],
